7f81092a65c5f01c5e682409b132b765c101fadd
harshitkaushik003 <harshitkaushik394@gmail.com>
2024-03-31 21:59:43 +0530

    added controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,10 +10,34 @@ module.exports = {
     },
     getUser: async (req, res)=>{
         try {
-            const user = await User.find({});
-            return res.status(200).json({data: user});
+            let query = {};
+
+            if(req.query.search){
+                query.first_name = req.query.search;
+            }
+            if(req.query.domain){
+                query.domain = req.query.domain;
+            }
+            if(req.query.gender){
+                query.gender = req.query.gender;
+            }
+            if(req.query.available){
+                query.available = req.query.available;
+            }
+            const user = await User.find(query);
+            return res.status(200).json({data: user, query: query});
+
         } catch (error) {
             return res.status(400).json({message: `an error occurred -> ${error}`})
         }
+    },
+    getUserById: async (req, res)=>{
+        try {
+            let user = await User.findById(req.params.id);
+            return res.status(200).json({data: user});
+        } catch (error) {
+            return res.status(200).json({error: error});
+        }
     }
+    
 }
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { create, getUser } = require('../controllers/userController');
+const { create, getUser} = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/', create);
 router.get('/', getUser);
+router.get('/:id', getUser);
 
 module.exports = router;
